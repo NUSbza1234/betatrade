a9a4c3d1c1648cbd39c3a5bac45cc5550c1968da
dimSum99 <144231175+NUSbza1234@users.noreply.github.com>
2024-06-05 01:20:17 +0800

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         
-        axios.post( 'https://mern-vercel-api-flame.vercel.app/register', {name, email, password}, {withCredentials: false})
+        axios.post( 'https://mern-vercel-api-flame.vercel.app/register', {name, email, password}, {withCredentials:true})
         .then(result => {
             console.log(result);
             if(result.data === "Already registered"){
